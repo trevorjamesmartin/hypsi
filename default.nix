d99e2d96d5db2fe644f444643835aadddcf0df0a
Trevor Martin <trevor.j.martin@gmail.com>
2024-10-18 15:44:01 -0400

    pin to revision

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cb9a96f23c491c081b38eab96d22fa958043c9fa.tar.gz") {config = { allowUnfree = true; }; } }:
 
 pkgs.callPackage ./derivation.nix {}
