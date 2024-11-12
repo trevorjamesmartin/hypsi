cd1777534c008101b9173fd7ad9051e40642cd6b
Trevor Martin <trevor.j.martin@gmail.com>
2024-11-12 13:24:51 -0500

    bump nixpkgs

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c0c77416acb81f698a292bae7bf3c1d524d421d9.tar.gz") {config = { allowUnfree = true; }; } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/dbd8326b7fa64379bdf3983c2fbde9029352c72f.tar.gz") {config = { allowUnfree = true; }; } }:
 
 pkgs.callPackage ./derivation.nix {}
