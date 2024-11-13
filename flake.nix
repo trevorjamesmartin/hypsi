17c325f267d01419fa8fc9a2e2cf0bc411d44360
NotAShelf <raf@notashelf.dev>
2024-11-13 19:56:50 +0300

    flake: use flake-compat for non-flakes compatibility

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
   description = "Simple tool to set your hyprpaper, written in Go";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
 
   outputs = {
     self,
     nixpkgs,
+    ...
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
